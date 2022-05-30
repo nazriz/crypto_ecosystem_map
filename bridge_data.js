e8829813a73d90e94183e72e9c234965e793d1d5
Nazim Rizvic <nazim.rizvic@me.com>
2022-05-30 13:16:26 +1000

    Added arbitrum bridge balance calculator

--- bridge_data.js
+++ bridge_data.js
@@ -11,24 +11,37 @@ const { daiContract, usdcContract, usdtContract, usdEthPriceContract } = require
 const usdc = usdcContract();
 const dai = daiContract();
 const usdt = usdtContract();
-const usdEthPrice = usdEthPriceContract();
+const usdEthPriceFeed = usdEthPriceContract();
 
 
 // async function main() {
-//     const usdcBalance = await usdc.balanceOf("0xcEe284F754E854890e311e3280b767F80797180d");
 //     console.log("The usdc balance is: " + ethers.utils.formatUnits(usdcBalance, 6));
-//     const usdtBalance = await usdt.balanceOf("0xcEe284F754E854890e311e3280b767F80797180d");
 //     console.log("The usdt balance is: " + ethers.utils.formatUnits(usdtBalance, 6));
 // }
 // main();
 
 
 const arbitrumBridgeBalance = async () => {
+    const arbitrumCustomGateway = "0xcEe284F754E854890e311e3280b767F80797180d" // ERC20's
+    const arbitrumWethGateway = "0x011B6E24FfB0B5f5fCc564cf4183C5BBBc96D515" // WETH
+    let usdcBalance = await usdc.balanceOf(arbitrumCustomGateway);
+    let usdtBalance = await usdt.balanceOf(arbitrumCustomGateway);
+    let ethBalance = await provider.getBalance(arbitrumWethGateway);
+    let usdEthPrice = await usdEthPriceFeed.latestAnswer();
 
+    usdcBalance = parseFloat(ethers.utils.formatUnits(usdcBalance, 6));
+    usdtBalance = parseFloat(ethers.utils.formatUnits(usdtBalance, 6));
+    ethBalance = parseFloat(ethers.utils.formatUnits(ethBalance, 18));
+    usdEthPrice = parseFloat(ethers.utils.formatUnits(usdEthPrice, 8));
+    ethPriceInUSD = (ethBalance * usdEthPrice)
+    bridgeTotalUSD = (usdcBalance + usdtBalance + ethPriceInUSD)
 
-    const arbEth = await provider.getBalance("0x011B6E24FfB0B5f5fCc564cf4183C5BBBc96D515");
-    console.log("The eth is: " + arbEth);
+    return bridgeTotalUSD
 }
 
 
-arbitrumBridgeBalance();
+let arb = arbitrumBridgeBalance();
+
+(async () => {
+    console.log(await arb);
+})()
