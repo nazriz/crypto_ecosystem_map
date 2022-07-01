3d6548f1db559ad376db5bc1741f818f7e6eaf4a
Nazim Rizvic <nazim.rizvic@me.com>
2022-07-01 16:47:20 +1000

    added Optimism token total value calculation

--- standalone_chains.js
+++ standalone_chains.js
@@ -8,6 +8,7 @@ const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
 const ALCHEMY_POLYGON_API_KEY = process.env.ALCHEMY_POLYGON_API_KEY;
 const MORALIS_AVALANCHE = process.env.MORALIS_AVALANCHE;
 const MORALIS_ARBITRUM = process.env.MORALIS_ARBITRUM;
+const ALCHEMY_OPTIMISM_API_KEY = process.env.ALCHEMY_OPTIMISM_API_KEY;
 
 const provider = new ethers.providers.InfuraProvider("homestead", {
   projectId: process.env.INFURA_PROJECT_ID,
@@ -15,6 +16,7 @@ const provider = new ethers.providers.InfuraProvider("homestead", {
 });
 
 const polygonProvider = new ethers.providers.AlchemyProvider("matic", ALCHEMY_POLYGON_API_KEY);
+const optimismProvider = new ethers.providers.AlchemyProvider("optimism", ALCHEMY_OPTIMISM_API_KEY);
 
 const avalancheProvider = new ethers.providers.JsonRpcProvider(MORALIS_AVALANCHE);
 const arbitrumProvider = new ethers.providers.JsonRpcProvider(MORALIS_ARBITRUM);
@@ -521,7 +523,7 @@ const avalancheTokenTotalValue = async () => {
     await tokenTotalSupply(avalancheProvider, "0x325a98F258a5732c7b06555603F6aF5BC1C17F0a", 9),
   ]));
 
-  console.log(tokens);
+  //   console.log(tokens);
 
   return tokens;
 };
@@ -645,15 +647,56 @@ const arbitrumTokenTotalValue = async () => {
   return tokens;
 };
 
-// avalancheTokenTotalValue();
+const optimismTokenTotalValue = async () => {
+  const tokens = ([
+    usdt,
+    usdc,
+    dai,
+    wbtc,
+    link,
+    frax,
+    fxs,
+    snx,
+    susd,
+    rgt,
+    seth,
+    sbtc,
+    thales,
+    dcn,
+    slink,
+    roobee,
+  ] = await Promise.all([
+    await tokenTotalSupply(optimismProvider, "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58", 6),
+    await tokenTotalSupply(optimismProvider, "0x7F5c764cBc14f9669B88837ca1490cCa17c31607", 6),
+    await tokenTotalSupply(optimismProvider, "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1", 18),
+    await tokenTotalSupply(optimismProvider, "0x68f180fcCe6836688e9084f035309E29Bf0A2095", 8),
+    await tokenTotalSupply(optimismProvider, "0x350a791Bfc2C21F9Ed5d10980Dad2e2638ffa7f6", 18),
+    await tokenTotalSupply(optimismProvider, "0x2E3D870790dC77A83DD1d18184Acc7439A53f475", 18),
+    await tokenTotalSupply(optimismProvider, "0x67CCEA5bb16181E7b4109c9c2143c24a1c2205Be", 18),
+    await tokenTotalSupply(optimismProvider, "0x8700dAec35aF8Ff88c16BdF0418774CB3D7599B4", 18),
+    await tokenTotalSupply(optimismProvider, "0x8c6f28f2F1A3C87F0f938b96d27520d9751ec8d9", 18),
+    await tokenTotalSupply(optimismProvider, "0xB548f63D4405466B36C0c0aC3318a22fDcec711a", 18),
+    await tokenTotalSupply(optimismProvider, "0xE405de8F52ba7559f9df3C368500B6E6ae6Cee49", 18),
+    await tokenTotalSupply(optimismProvider, "0x298B9B95708152ff6968aafd889c6586e9169f1D", 18),
+    await tokenTotalSupply(optimismProvider, "0x217D47011b23BB961eB6D93cA9945B7501a5BB11", 18),
+    await tokenTotalSupply(optimismProvider, "0x1da650C3B2DaA8AA9Ff6F661d4156Ce24d08A062", 18),
+    await tokenTotalSupply(optimismProvider, "0xc5Db22719A06418028A40A9B5E9A7c02959D0d08", 18),
+    await tokenTotalSupply(optimismProvider, "0xb12c13e66AdE1F72f71834f2FC5082Db8C091358", 18),
+  ]));
+
+  //   console.log(tokens);
+  return tokens;
+};
 
 const test = async () => {
+  let optimismTokens = optimismTokenTotalValue();
   let arbitrumTokens = arbitrumTokenTotalValue();
   //   let polygonTokens = await polygonTotalTokenValue();
   //   let avaxTokens = await avalancheTokenTotalValue();
   //   getPrices("polygon-pos", polygonTokens);
   //   getPrices("avalanche", avaxTokens);
-  getPrices("arbitrum-one", arbitrumTokens);
+  getPrices("arbitrum-one", await arbitrumTokens);
+  //   getPrices("optimistic-ethereum", await optimismTokens);
 };
 
 test();
