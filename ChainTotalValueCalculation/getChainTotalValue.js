c108ef07a692bc9bdc04e48c3e03922b3131a831
Nazim Rizvic <nazim.rizvic@me.com>
2022-07-14 12:17:57 +1000

    Added total mcap calculation function for tokens based on provided coingecko id

--- ChainTotalValueCalculation/getChainTotalValue.js
+++ ChainTotalValueCalculation/getChainTotalValue.js
@@ -37,61 +37,105 @@ for (const [key, value] of Object.entries(chainTotalValue)) {
 }
 
 const calculateChainValue = async () => {
-  let [ethereumTokens, optimismTokens, arbitrumTokens, polygonTokens, avalancheTokens, bnbTokens] = await Promise.all([
-    ethereumTokenTotalValue(),
-    optimismTokenTotalValue(),
-    arbitrumTokenTotalValue(),
-    polygonTotalTokenValue(),
-    avalancheTokenTotalValue(),
-    bnbTotalTokenValue(),
-  ]);
+  // let [ethereumTokens, optimismTokens, arbitrumTokens, polygonTokens, avalancheTokens, bnbTokens] = await Promise.all([
+  //   ethereumTokenTotalValue(),
+  //   optimismTokenTotalValue(),
+  //   arbitrumTokenTotalValue(),
+  //   polygonTotalTokenValue(),
+  //   avalancheTokenTotalValue(),
+  //   bnbTotalTokenValue(),
+  // ]);
 
-  let [
-    ethereumTotalValue,
-    optimismTotalValue,
-    arbitrumTotalValue,
-    polygonTotalValue,
-    avalancheTotalValue,
-    bnbTotalValue,
-    solanaTotalValue,
-  ] = await Promise.all([
-    getPrices("ethereum", ethereumTokens),
-    getPrices("optimistic-ethereum", optimismTokens),
-    getPrices("arbitrum-one", arbitrumTokens),
-    getPrices("polygon-pos", polygonTokens),
-    getPrices("avalanche", avalancheTokens),
-    getPrices("binance-smart-chain", bnbTokens),
-    solanaTokenValue(),
-  ]);
+  // let [
+  //   ethereumTotalValue,
+  //   optimismTotalValue,
+  //   arbitrumTotalValue,
+  //   polygonTotalValue,
+  //   avalancheTotalValue,
+  //   bnbTotalValue,
+  //   solanaTotalValue,
+  // ] = await Promise.all([
+  //   getPrices("ethereum", ethereumTokens),
+  //   getPrices("optimistic-ethereum", optimismTokens),
+  //   getPrices("arbitrum-one", arbitrumTokens),
+  //   getPrices("polygon-pos", polygonTokens),
+  //   getPrices("avalanche", avalancheTokens),
+  //   getPrices("binance-smart-chain", bnbTokens),
+  //   solanaTokenValue(),
+  // ]);
 
-  let [ethMcap] = await Promise.all([]);
+  let ethMcap,
+    opMcap,
+    avaxMcap,
+    maticMcap = 0;
 
-  ethereumTotal["ethereum"] = ethereumTotalValue;
+  //Add coingecko ID to this array, for it to be added to the request payload
+  // Must additionally add logic to the for loop below for the mcap
+  // to be included in final calculation
+  let coingeckoMcapIds = ["avalanche-2", "matic-network"];
 
-  layer2Totals["optimism"] = optimismTotalValue;
-  layer2Totals["arbitrum"] = arbitrumTotalValue;
+  coingeckoMcapPayload = "";
 
-  sidechainTotals["polygon"] = polygonTotalValue;
+  for (id in coingeckoMcapIds) {
+    coingeckoMcapPayload += "%2C%20" + coingeckoMcapIds[id];
+  }
 
-  altL1Totals["avalanche"] = avalancheTotalValue;
-  altL1Totals["BNB"] = bnbTotalValue;
-  altL1Totals["solana"] = solanaTotalValue;
+  let geckoData = await axios.get(
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum${coingeckoMcapPayload}&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+  );
 
-  chainTotalValue["ethereum"] = ethereumTotal;
-  chainTotalValue["layer2"] = layer2Totals;
-  chainTotalValue["sidechain"] = sidechainTotals;
-  chainTotalValue["alt_l1"] = altL1Totals;
+  let data = await geckoData["data"];
+
+  for (item in data) {
+    let chain = data[item];
+    if (chain["symbol"] === "eth") {
+      ethMcap = chain["market_cap"];
+    } else if (chain["symbol"] === "avax") {
+      avaxMcap = chain["market_cap"];
+    } else if (chain["symbol"] === "matic") {
+      maticMcap = chain["market_cap"];
+    }
+  }
+
+  // ethereumTotal["ethereum"] = ethereumTotalValue;
+
+  // layer2Totals["optimism"] = optimismTotalValue;
+  // layer2Totals["arbitrum"] = arbitrumTotalValue;
+
+  // sidechainTotals["polygon"] = polygonTotalValue;
+
+  // altL1Totals["avalanche"] = avalancheTotalValue;
+  // altL1Totals["BNB"] = bnbTotalValue;
+  // altL1Totals["solana"] = solanaTotalValue;
+
+  // chainTotalValue["ethereum"] = ethereumTotal;
+  // chainTotalValue["layer2"] = layer2Totals;
+  // chainTotalValue["sidechain"] = sidechainTotals;
+  // chainTotalValue["alt_l1"] = altL1Totals;
   let dataToWrite = JSON.stringify(chainTotalValue);
   console.log(dataToWrite);
   fs.writeFileSync("../chainTotalValue.json", dataToWrite);
 };
 
-// calculateChainValue();
+calculateChainValue();
 
 const test = async () => {
-  let geckoData = axios.get(
+  let geckoData = await axios.get(
     `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ethereum%2C%20avalanche-2%2C%20matic-network&order=market_cap_desc&per_page=100&page=1&sparkline=false`
   );
 
-  console.log(geckoData);
+  let data = await geckoData["data"];
+
+  console.log(data);
+
+  for (item in data) {
+    let chain = data[item];
+    if (chain["symbol"] == "eth") {
+      console.log("Found it!");
+    } else {
+      console.log("Not Eth!");
+    }
+  }
 };
+
+// test();
