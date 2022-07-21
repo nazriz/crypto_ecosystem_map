8ff5a6964c74d236b0f9fb622cc87b52479cba4c
Nazim Rizvic <nazim.rizvic@me.com>
2022-07-21 17:47:14 +1000

    file creation  still not working as anticipated, will need to review logic

--- generateFinalJSON.js
+++ generateFinalJSON.js
@@ -66,3 +66,80 @@ const getUniqueChainNames = () => {
 };
 
 let uniqueChainNames = getUniqueChainNames();
+let finalChainArray = [];
+let tempChainArray = [];
+
+for (item in uniqueChainNames) {
+  finalChainArray.push(tempChainArray);
+  tempChainArray = [];
+  let chainName = uniqueChainNames[item];
+  for (type in bridgedFromEth) {
+    let tempBridgedFromEth = bridgedFromEth[type];
+    for (chain in tempBridgedFromEth) {
+      if (chain === chainName) {
+        tempChainArray.push(tempBridgedFromEth[chain]);
+      }
+    }
+  }
+
+  for (type2 in chainTokenMcap) {
+    let tempChainTokenMcap = chainTokenMcap[type2];
+    for (chain2 in tempChainTokenMcap) {
+      if (chain2 === chainName) {
+        // tempChainArray.push(tempChainTokenMcap[chain]);
+        console.log(chain2, chainName);
+      }
+    }
+  }
+}
+
+// console.log(finalChainArray);
+
+// let cleanArray = finalChainArray.filter((e) => e.length);
+// console.log(cleanArray);
+
+// let finalChainArray = [];
+// let tempChainArray = [];
+// let tempChainObj = {};
+// let tempTypeObj = {};
+// for (type in bridgedFromEth) {
+//   for (type2 in chainTokenMcap) {
+//     for (type3 in ecosystemValue) {
+//       if (type === type2 && type === type3) {
+//         let tempBridgedFromEth = bridgedFromEth[type];
+//         let tempChainTokenMcap = chainTokenMcap[type];
+//         let tempEcosystemValue = ecosystemValue[type];
+//         for (chain in tempBridgedFromEth) {
+//           let chainObjName = chain;
+//           for (chain2 in tempChainTokenMcap) {
+//             for (chain3 in tempEcosystemValue) {
+//               tempChainArray = [];
+
+//               for (chainName in uniqueChainNames) {
+//                 if (chain === uniqueChainNames[chainName]) {
+//                   tempChainArray.push(tempBridgedFromEth[chain]);
+//                   tempChainArray.push(tempChainTokenMcap[chain2]);
+//                   tempChainArray.push(tempEcosystemValue[chain3]);
+
+//                   // }
+//                   // if (chain2 === uniqueChainNames[chainName]) {
+//                   //   tempChainArray.push(tempChainTokenMcap[chain2]);
+//                   // }
+//                   // if (chain3 === uniqueChainNames[chainName]) {
+//                   //   tempChainArray.push(tempEcosystemValue[chain3]);
+//                   // }
+//                 }
+//               }
+//               console.log(tempChainArray);
+//               console.log("next");
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+
+// let testObj = { ...chainTokenMcap, ...bridgedFromEth, ...ecosystemValue };
+
+console.log(chainTokenMcap, bridgedFromEth, ecosystemValue);
