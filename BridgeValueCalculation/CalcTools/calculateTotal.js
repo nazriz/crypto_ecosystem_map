0ead4ce2f10a2ced7d36616556e143812e7f23db
Nazim Rizvic <nazim.rizvic@me.com>
2022-09-22 13:00:51 +1000

    added handling for undefined price

--- BridgeValueCalculation/CalcTools/calculateTotal.js
+++ BridgeValueCalculation/CalcTools/calculateTotal.js
@@ -1,13 +1,12 @@
 const calculateTotal = (inputBridge, priceFeed) => {
   let runningTotal = 0.0;
   for (const item in inputBridge) {
-    if (item != "USD") {
+    if (item != "USD" && item in priceFeed) {
       runningTotal += inputBridge[item] * priceFeed[item];
     }
   }
   let total = inputBridge["USD"] + runningTotal;
 
-  //   total = total.toFixed(0);
   return total;
 };
 
