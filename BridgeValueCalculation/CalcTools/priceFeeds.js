1b4d3d4faa38047f371f20988bfb035ee1ee8bbc
Nazim Rizvic <nazim.rizvic@me.com>
2022-09-22 13:02:15 +1000

    changed centralised feed structure from requesting per feed, to aggregating all feeds and making a single request. This is to avoid rate limiting from API

--- BridgeValueCalculation/CalcTools/priceFeeds.js
+++ BridgeValueCalculation/CalcTools/priceFeeds.js
@@ -2,11 +2,14 @@ const { ethers } = require("ethers");
 require("dotenv").config();
 const axios = require("axios");
 const API_KEY = process.env.API_KEY;
+const axiosThrottle = require("axios-request-throttle");
 
 const provider = new ethers.providers.InfuraProvider("homestead", {
   projectId: process.env.INFURA_PROJECT_ID,
   projectSecret: process.env.INFURA_PROJECT_SECRET,
 });
+
+axiosThrottle.use(axios, { requestsPerSecond: 1 });
 
 const { ethUsdPriceContract, btcUsdPriceContract, feedRegistry } = require("./contractObjects");
 
@@ -55,34 +58,6 @@ const priceFeeds = async () => {
     lrcPrice,
     imxPrice,
     omgPrice,
-    ghst,
-    dg,
-    xdg,
-    awx,
-    bel,
-    cel,
-    woo,
-    alpha,
-    aleph,
-    serum,
-    xcn,
-    nexm,
-    ldo,
-    ice,
-    woofy,
-    axs,
-    iceth,
-    omi,
-    dvf,
-    hez,
-    metis,
-    boba,
-    zks,
-    cdai,
-    hopr,
-    dxd,
-    gno,
-    node,
   ] = await Promise.all([
     parseFloat(ethers.utils.formatUnits(await ethUsdPriceFeed.latestAnswer(), 8)),
     parseFloat(ethers.utils.formatUnits(await btcUsdPriceFeed.latestAnswer(), 8)),
@@ -101,38 +76,50 @@ const priceFeeds = async () => {
     await feedingRegistry.latestRoundData(lrcTokenAddress, USD),
     await feedingRegistry.latestRoundData(imxTokenAddress, USD),
     await feedingRegistry.latestRoundData(omgTokenAddress, USD),
-    //Centralised Feeds
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=aavegotchi&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=decentral-games&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=decentral-games-governance&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=auruscoin&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=bella-protocol&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=celsius-degree-token&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=woo-network&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=alpha-finance&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=aleph&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=serum&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=chain-2&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=nexum&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=lido-dao&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=ice-token&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=woofy&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=axie-infinity&vs_currencies=usd`),
-    await axios.get(
-      `https://api.coingecko.com/api/v3/simple/price?ids=interest-compounding-eth-index&vs_currencies=usd`
-    ),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=ecomi&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=rhinofi&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=hermez-network-token&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=metis-token&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=boba-network&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=zkspace&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=cdai&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=hopr&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=dxdao&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=gnosis&vs_currencies=usd`),
-    await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=dappnode&vs_currencies=usd`),
   ]);
+
+  // For coingecko pricefeeds
+  tickerAddress = {
+    GHST: "0x3f382dbd960e3a9bbceae22651e88158d2791550",
+    DG: "0x4b520c812e8430659fc9f12f6d0c39026c83588d",
+    XDG: "0x4f81c790581b240a5c948afd173620ecc8c71c8d",
+    AWX: "0xa51fc71422a30fa7ffa605b360c3b283501b5bf6",
+    BEL: "0xa91ac63d040deb1b7a5e4d4134ad23eb0ba07e14",
+    CEL: "0xaaaebe6fe48e54f431b0c390cfaf0b017d09d42d",
+    WOO: "0x4691937a7508860f876c9c0a2a617e7d9e945d4b",
+    ALPHA: "0xa1faa113cbe53436df28ff0aee54275c13b40975",
+    ALEPH: "0x27702a26126e0b3702af63ee09ac4d1a084ef628",
+    SRM: "0x476c5e26a75bd202a9683ffd34359c0cc15be0ff",
+    XCN: "0xa2cd3d43c775978a96bdbf12d733d5a1ed94fb18",
+    NEXM: "0xe831f96a7a1dce1aa2eb760b1e296c6a74caa9d5",
+    LDO: "0x5a98fcbea516cf06857215779fd812ca3bef1b32",
+    ICE: "0xf16e81dce15b08f326220742020379b855b87df9",
+    WOOFY: "0xd0660cd418a64a1d44e9214ad8e459324d8157f1",
+    AXS: "0xbb0e17ef65f82ab018d8edd776e8dd940327b28b",
+    icETH: "0x7c07f7abe10ce8e33dc6c5ad68fe033085256a84",
+    OMI: "0xed35af169af46a02ee13b9d79eb57d6d68c1749e",
+    DVF: "0xdddddd4301a082e62e84e43f474f044423921918",
+    HEZ: "0xeef9f339514298c6a857efcfc1a762af84438dee",
+    METIS: "0x9e32b13ce7f2e80a01932b42553652e053d6ed8e",
+    BOBA: "0x42bbfa2e77757c645eeaad1655e0911a7553efbc",
+    ZKS: "0xe4815ae53b124e7263f08dcdbbb757d41ed658c6",
+    cDAI: "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643",
+    HOPR: "0xf5581dfefd8fb0e4aec526be659cfab1f8c781da",
+    DXD: "0xa1d65e8fb6e87b60feccbc582f7f97804b725521",
+    GNO: "0x6810e776880c02933d47db1b9fc05908e5386b96",
+    NODE: "0xda007777d86ac6d989cc9f79a73261b3fc5e0da0",
+  };
+
+  let geckoPayload = "";
+  for (const [ticker, address] of Object.entries(tickerAddress)) {
+    geckoPayload += address + ",";
+  }
+
+  let geckoData = await axios.get(
+    `https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=${geckoPayload}&vs_currencies=usd`
+  );
+
+  let cleanGeckoData = geckoData["data"];
 
   priceFeeds["ETH"] = ethPrice;
   priceFeeds["WETH"] = ethPrice;
@@ -198,44 +185,17 @@ const priceFeeds = async () => {
   );
 
   // Centralised Feeds
-  //Aavegotchi
-  priceFeeds["GHST"] = parseFloat(ghst["data"]["aavegotchi"]["usd"]);
-  // Decentral Games
-  priceFeeds["DG"] = parseFloat(dg["data"]["decentral-games"]["usd"]);
-  // Decentral Games Governance
-  priceFeeds["XDG"] = parseFloat(xdg["data"]["decentral-games-governance"]["usd"]);
-  // Aurus Defi
-  priceFeeds["AWX"] = parseFloat(awx["data"]["auruscoin"]["usd"]);
-  // Bella Protocol
-  priceFeeds["BEL"] = parseFloat(bel["data"]["bella-protocol"]["usd"]);
-  // Celsius Network
-  priceFeeds["CEL"] = parseFloat(cel["data"]["celsius-degree-token"]["usd"]);
-  // Woo Network
-  priceFeeds["WOO"] = parseFloat(woo["data"]["woo-network"]["usd"]);
-  priceFeeds["ALPHA"] = parseFloat(alpha["data"]["alpha-finance"]["usd"]);
-  priceFeeds["ALEPH"] = parseFloat(aleph["data"]["aleph"]["usd"]);
-  priceFeeds["SRM"] = parseFloat(serum["data"]["serum"]["usd"]);
-  priceFeeds["XCN"] = parseFloat(xcn["data"]["chain-2"]["usd"]);
-  priceFeeds["NEXM"] = parseFloat(nexm["data"]["nexum"]["usd"]);
-  priceFeeds["LDO"] = parseFloat(ldo["data"]["lido-dao"]["usd"]);
-  priceFeeds["ICE"] = parseFloat(ice["data"]["ice-token"]["usd"]);
-  priceFeeds["WOOFY"] = parseFloat(woofy["data"]["woofy"]["usd"]);
-  priceFeeds["AXS"] = parseFloat(axs["data"]["axie-infinity"]["usd"]);
-  priceFeeds["icETH"] = parseFloat(iceth["data"]["interest-compounding-eth-index"]["usd"]);
-  priceFeeds["OMI"] = parseFloat(omi["data"]["ecomi"]["usd"]);
-  priceFeeds["DVF"] = parseFloat(dvf["data"]["rhinofi"]["usd"]);
-  priceFeeds["xDVF"] = parseFloat(dvf["data"]["rhinofi"]["usd"]);
-  priceFeeds["HEZ"] = parseFloat(hez["data"]["hermez-network-token"]["usd"]);
-  priceFeeds["METIS"] = parseFloat(metis["data"]["metis-token"]["usd"]);
-  priceFeeds["BOBA"] = parseFloat(boba["data"]["boba-network"]["usd"]);
-  priceFeeds["ZKS"] = parseFloat(zks["data"]["zkspace"]["usd"]);
-  priceFeeds["cDAI"] = parseFloat(cdai["data"]["cdai"]["usd"]);
-  priceFeeds["HOPR"] = parseFloat(hopr["data"]["hopr"]["usd"]);
-  priceFeeds["DXD"] = parseFloat(dxd["data"]["dxdao"]["usd"]);
-  priceFeeds["GNO"] = parseFloat(gno["data"]["gnosis"]["usd"]);
-  priceFeeds["NODE"] = parseFloat(node["data"]["dappnode"]["usd"]);
 
-  // console.log(priceFeeds);
+  for (const [xkey1, xvalue1] of Object.entries(cleanGeckoData)) {
+    for (const [xkey2, xvalue2] of Object.entries(tickerAddress)) {
+      if (xkey1 === xvalue2.toLowerCase()) {
+        let temp = cleanGeckoData[xkey1];
+        let price = parseFloat(temp["usd"]);
+        priceFeeds[xkey2] = price;
+      }
+    }
+  }
+
   return priceFeeds;
 };
 
