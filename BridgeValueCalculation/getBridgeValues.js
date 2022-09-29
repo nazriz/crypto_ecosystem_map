68abb639bf74b6a02917d5b0e3dc747e091a8297
Nazim Rizvic <nazim.rizvic@me.com>
2022-09-29 13:04:00 +1000

    debug

--- BridgeValueCalculation/getBridgeValues.js
+++ BridgeValueCalculation/getBridgeValues.js
@@ -22,42 +22,42 @@ const getBridgeValues = async () => {
   console.log("Waiting 5 seconds...");
   timer(5000);
 
-  // console.log("opening layer 2 file");
-  // let layer2File = fs.readFileSync(path.resolve(__dirname, "../data/bridgedFromEthToLayer2.json"), (err) => {
-  //   if (err) {
-  //     console.error(err);
-  //   }
-  // });
-  // let layer2Data = JSON.parse(layer2File);
+  console.log("opening layer 2 file");
+  let layer2File = fs.readFileSync(path.resolve(__dirname, "../data/bridgedFromEthToLayer2.json"), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+  let layer2Data = JSON.parse(layer2File);
 
-  // console.log("opening altl1file");
-  // let altL1File = fs.readFileSync(path.resolve(__dirname, "../data/bridgedFromEthToAltL1.json"), (err) => {
-  //   if (err) {
-  //     console.error(err);
-  //   }
-  // });
-  // let altL1Data = JSON.parse(altL1File);
+  console.log("opening altl1file");
+  let altL1File = fs.readFileSync(path.resolve(__dirname, "../data/bridgedFromEthToAltL1.json"), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+  let altL1Data = JSON.parse(altL1File);
 
-  // console.log("opening sidechain file");
-  // let sidechainFile = fs.readFileSync(path.resolve(__dirname, "../data/bridgedFromEthToSidechain.json"), (err) => {
-  //   if (err) {
-  //     console.error(err);
-  //   }
-  // });
-  // let sidechainData = JSON.parse(sidechainFile);
+  console.log("opening sidechain file");
+  let sidechainFile = fs.readFileSync(path.resolve(__dirname, "../data/bridgedFromEthToSidechain.json"), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+  let sidechainData = JSON.parse(sidechainFile);
 
-  // let combinedBridges = {};
+  let combinedBridges = {};
 
-  // combinedBridges["layer2"] = layer2Data["layer2"];
-  // combinedBridges["sidechain"] = sidechainData["sidechain"];
-  // combinedBridges["altL1"] = altL1Data["altL1"];
+  combinedBridges["layer2"] = layer2Data["layer2"];
+  combinedBridges["sidechain"] = sidechainData["sidechain"];
+  combinedBridges["altL1"] = altL1Data["altL1"];
 
-  // fs.writeFile(path.resolve(__dirname, "../data/bridgedFromEth.json"), JSON.stringify(combinedBridges), (err) => {
-  //   if (err) {
-  //     console.error(err);
-  //   }
-  // });
-  // console.log("complete!");
+  fs.writeFile(path.resolve(__dirname, "../data/bridgedFromEth.json"), JSON.stringify(combinedBridges), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+  console.log("complete!");
 };
 
 // called from updateData.py
